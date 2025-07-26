b8bcc5c5494a62f9bacf03160f385c41c7c36874
Henry Chukwu <hchukwu@Henrys-MacBook-Pro.local>
2025-07-26 11:50:27 +0100

    Initial Commit

---
+++ KYC-Flow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KYC-Flow
+//
+//  Created by Henry Chukwu on 26/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KYC-Flow/KYC_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  KYC_FlowApp.swift
+//  KYC-Flow
+//
+//  Created by Henry Chukwu on 26/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct KYC_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
